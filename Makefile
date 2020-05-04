54bb9da34f10c63898591169d5e147f72285bcf2
Xabier Laiseca <xabier@laiseca.me>
2020-05-04 18:33:13 +0100

    formatting & vetting as part of the build

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-BIN_DIR := bin
+BIN_DIR := ./bin
 .PHONY: build build-gowrap build-init clean test
+
+pre-build:
+	go fmt ./...
+	go vet ./...
 
 build-init:
 	mkdir -p $(BIN_DIR)
@@ -7,10 +11,14 @@ build-init:
 build-gowrap: build-init
 	go build -o $(BIN_DIR)/gowrap cmd/gowrap/main.go
 
-build: build-gowrap
+build: pre-build test build-gowrap
 
 clean:
 	rm -r $(BIN_DIR)
 
 test:
 	go test -v ./...
+
+generate-versions-file: build-gowrap
+	$(BIN_DIR)/gowrap versions-file generate --file data/versions.json
+	
--- pkg/cache/local_test.go
+++ pkg/cache/local_test.go
@@ -1,13 +1,13 @@
 package cache
 
 import (
+	"os"
 	"testing"
 	"time"
-  "os"
 
-  "io/ioutil"
-  "github.com/stretchr/testify/assert"
-  "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
 )
 
 type Verification func(*testing.T, string, string)
@@ -18,64 +18,64 @@ func Test_Cache(t *testing.T) {
 		verifyFiles map[string]Verification
 	}{
 		"SetAndGetOne": {
-		  cache: map[string]time.Duration{
-        "test.txt": (60 * time.Minute),
-      },
-      verifyFiles: map[string]Verification {
-        "test.txt": verifyExists,
-      },
+			cache: map[string]time.Duration{
+				"test.txt": (60 * time.Minute),
+			},
+			verifyFiles: map[string]Verification{
+				"test.txt": verifyExists,
+			},
 		},
-    "GetNotExists": {
-		  cache: map[string]time.Duration{},
-      verifyFiles: map[string]Verification {
-        "test.txt": verifyNotExists,
-      },
+		"GetNotExists": {
+			cache: map[string]time.Duration{},
+			verifyFiles: map[string]Verification{
+				"test.txt": verifyNotExists,
+			},
 		},
-    "SetAndGetUntilExpires": {
-		  cache: map[string]time.Duration{
-        "test.txt": (50 * time.Millisecond),
-      },
-      verifyFiles: map[string]Verification {
-        "test.txt": verifyExpires,
-      },
+		"SetAndGetUntilExpires": {
+			cache: map[string]time.Duration{
+				"test.txt": (50 * time.Millisecond),
+			},
+			verifyFiles: map[string]Verification{
+				"test.txt": verifyExpires,
+			},
 		},
 	}
 
 	for testName, testCase := range testCases {
 		t.Run(testName, func(t *testing.T) {
-      tmpDir, err := ioutil.TempDir(os.TempDir(), "test-cache-")
-      require.NoError(t, err)
+			tmpDir, err := ioutil.TempDir(os.TempDir(), "test-cache-")
+			require.NoError(t, err)
 
 			for relpath, expiresIn := range testCase.cache {
-        err := set(tmpDir, relpath, []byte(relpath), expiresIn)
-        assert.NoError(t, err)
+				err := set(tmpDir, relpath, []byte(relpath), expiresIn)
+				assert.NoError(t, err)
 			}
 
-      for relpath, verify := range testCase.verifyFiles {
-        verify(t, tmpDir, relpath)
-      }
+			for relpath, verify := range testCase.verifyFiles {
+				verify(t, tmpDir, relpath)
+			}
 		})
 	}
 }
 
 func verifyExists(t *testing.T, rootDir, relpath string) {
-  actual, err := get(rootDir, relpath)
-  assert.NoError(t, err)
-  assert.Equal(t, []byte(relpath), actual)
+	actual, err := get(rootDir, relpath)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(relpath), actual)
 }
 
 func verifyNotExists(t *testing.T, rootDir, relpath string) {
-  actual, err := get(rootDir, relpath)
-  assert.NoError(t, err)
-  assert.Nil(t, actual)
+	actual, err := get(rootDir, relpath)
+	assert.NoError(t, err)
+	assert.Nil(t, actual)
 }
 
 func verifyExpires(t *testing.T, rootDir, relpath string) {
-  verifyExists(t, rootDir, relpath)
-  cacheEntryNotExists := func() bool {
-    content, err := get(rootDir, relpath)
-    return content == nil && err == nil
-  }
+	verifyExists(t, rootDir, relpath)
+	cacheEntryNotExists := func() bool {
+		content, err := get(rootDir, relpath)
+		return content == nil && err == nil
+	}
 
-  assert.Eventually(t, cacheEntryNotExists, 1 * time.Second, 50 * time.Millisecond)
+	assert.Eventually(t, cacheEntryNotExists, 1*time.Second, 50*time.Millisecond)
 }
