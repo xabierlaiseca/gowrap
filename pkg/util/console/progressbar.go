2192964f852d44bfe8be0b421a0f78e6af9dd2d6
Xabier Laiseca <xlaiseca@confluent.io>
2020-05-23 18:54:15 +0100

    upgrade progress bar also on downloaded size change, not only progress line

--- pkg/util/console/progressbar.go
+++ pkg/util/console/progressbar.go
@@ -15,27 +15,32 @@ type ProgressBar interface {
 type Printer func(int64) string
 
 func NewProgressBar(max int64, printer Printer) ProgressBar {
-	cpb := consoleProgressBar{
-		maxValue:        max,
-		currentValue:    0,
-		maxProgress:     defaultProgressBarLength,
-		currentProgress: 0,
-		valuePrinter:    printer,
-		maxValueString:  printer(max),
+	cpb := &consoleProgressBar{
+		maxValue:         max,
+		currentValue:     0,
+		maxProgress:      defaultProgressBarLength,
+		currentProgress:  0,
+		valuePrinter:     printer,
+		maxFriendlyValue: printer(max),
 	}
 
-	fmt.Print(cpb.buildConsoleLine())
-	return &cpb
+	cpb.printProgressBar()
+	return cpb
 }
 
 type consoleProgressBar struct {
-	maxValue        int64
-	currentValue    int64
-	maxProgress     int
-	currentProgress int
-	valuePrinter    Printer
+	currentValue int64
+	maxValue     int64
 
-	maxValueString string
+	currentProgress int
+	printedProgress int
+	maxProgress     int
+
+	currentFriendlyValue string
+	printedFriendlyValue string
+	maxFriendlyValue     string
+
+	valuePrinter Printer
 }
 
 func (cpb *consoleProgressBar) Increment(inc int64) {
@@ -45,13 +50,7 @@ func (cpb *consoleProgressBar) Increment(inc int64) {
 		cpb.currentValue += inc
 	}
 
-	previousProgress := cpb.currentProgress
-	cpb.currentProgress = int(int64(cpb.maxProgress) * cpb.currentValue / cpb.maxValue)
-
-	if cpb.currentProgress > previousProgress {
-		line := cpb.buildConsoleLine()
-		fmt.Printf("\r\033[K%s", line)
-	}
+	cpb.printProgressBar()
 }
 
 func (cpb *consoleProgressBar) Done() {
@@ -62,17 +61,32 @@ func (cpb *consoleProgressBar) Done() {
 	fmt.Println()
 }
 
-func (cpb *consoleProgressBar) buildConsoleLine() string {
-	greaterSigns := 1
-	if cpb.currentProgress == cpb.maxProgress {
-		greaterSigns = 0
-	}
-	spaces := cpb.maxProgress - cpb.currentProgress - greaterSigns
+func (cpb *consoleProgressBar) printProgressBar() {
+	cpb.currentProgress = int(int64(cpb.maxProgress) * cpb.currentValue / cpb.maxValue)
+	cpb.currentFriendlyValue = cpb.valuePrinter(cpb.currentValue)
 
-	return fmt.Sprintf("[%s%s%s] %s/%s",
-		strings.Repeat("=", cpb.currentProgress),
-		strings.Repeat(">", greaterSigns),
-		strings.Repeat(" ", spaces),
-		cpb.valuePrinter(cpb.currentValue),
-		cpb.maxValueString)
+	if cpb.needsUpdate() {
+		greaterSigns := 1
+		if cpb.currentProgress == cpb.maxProgress {
+			greaterSigns = 0
+		}
+		spaces := cpb.maxProgress - cpb.currentProgress - greaterSigns
+
+		line := fmt.Sprintf("\r\033[K[%s%s%s] %s/%s",
+			strings.Repeat("=", cpb.currentProgress),
+			strings.Repeat(">", greaterSigns),
+			strings.Repeat(" ", spaces),
+			cpb.currentFriendlyValue,
+			cpb.maxFriendlyValue)
+
+		fmt.Print(line)
+
+		cpb.printedProgress = cpb.currentProgress
+		cpb.printedFriendlyValue = cpb.currentFriendlyValue
+	}
+}
+
+func (cpb *consoleProgressBar) needsUpdate() bool {
+	return cpb.printedProgress != cpb.currentProgress ||
+		strings.Compare(cpb.printedFriendlyValue, cpb.currentFriendlyValue) != 0
 }
